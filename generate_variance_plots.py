Fixed variance plots

generate_variance_plots.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
 from data_functions import *
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import os
@@ -14,9 +16,11 @@ def plot_variance():
     samples_flow_2 = torch.load("sampled_images/flow_2/samples_flow_2.pt")
     # Find the variance
     # scale to be between 0 and 1
-    images_org = images_org.float()/255
-    samples_flow_1 = samples_flow_1.float()/255
-    samples_flow_2 = samples_flow_2.float()/255
+    images_org = (images_org.float()/255).squeeze()
+    samples_flow_1 = (samples_flow_1.float()/255).squeeze()
+    samples_flow_2 = (samples_flow_2.float()/255).squeeze()
+    #squeeze the images
+
 
     variance_images = torch.var(images_org.float(), dim=0)
     variance_flow_1 = torch.var(samples_flow_1, dim=0)
@@ -24,18 +28,25 @@ def plot_variance():
     # Visualize the variance in subplots
     fig, axs = plt.subplots(1, 3)
 
+    # set the min and max values for the colorbar
+    vmin = min(variance_images.min(), variance_flow_1.min(), variance_flow_2.min())
+    vmax = max(variance_images.max(), variance_flow_1.max(), variance_flow_2.max())
+    norm = Normalize(vmin=vmin, vmax=vmax)
+
+    shrink_value = 0.4
     # Original
-    im0 = axs[0].imshow(variance_images.detach().numpy())
+    im0 = axs[0].imshow(variance_images.detach().numpy(), norm=norm)
     axs[0].set_title("Original")
-    cbar0 = fig.colorbar(im0, ax=axs[0], fraction=0.046, pad=0.04)
+    fig.colorbar(im0, ax=axs[0], orientation='vertical', shrink=shrink_value)
     # Flow 1
-    im1 = axs[1].imshow(variance_flow_1[0].detach().numpy())
+    im1 = axs[1].imshow(variance_flow_1.detach().numpy(), norm=norm)
     axs[1].set_title("Flow 1")
-    cbar1 = fig.colorbar(im1, ax=axs[1], fraction=0.046, pad=0.04)
+    fig.colorbar(im1, ax=axs[1], orientation='vertical', shrink=shrink_value)
     # Flow 2
-    im2 = axs[2].imshow(variance_flow_2[0].detach().numpy())
+    im2 = axs[2].imshow(variance_flow_2.detach().numpy(), norm=norm)
     axs[2].set_title("Flow 2")
-    cbar2 = fig.colorbar(im2, ax=axs[2], fraction=0.046, pad=0.04)
+    fig.colorbar(im2, ax=axs[2], orientation='vertical', shrink=shrink_value)
+
     #remove ticks form all subplots
     for ax in axs:
         ax.set_xticks([])